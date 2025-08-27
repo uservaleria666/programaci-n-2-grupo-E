f14382e70d8b05f6aa9ff67f38a3c2c7c8c4cf31
uservaleria666 <guzmanvaleria232@gmail.com>
2025-08-27 08:37:04 -0400

    Add files via upload

---
+++ spinbox.py
@@ -0,0 +1,15 @@
+#spinbox de numeros del 1 al 10 para edad
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+def mostrarEdad():
+    tk.messagebox.showinfo("Edad",f"La edad seleccionada es :{spin.get()}")
+    
+ventana=tk.Tk() 
+labelEdad=tk.Label(ventana, text="Edad")
+labelEdad.grid(row=0, column=0, padx=5 ,pady=5, sticky="w")
+spin=tk.Spinbox(ventana,from_=1, to=10)
+spin.grid(row=0, column=1,padx=10,pady=10)
+boton=tk.Button(ventana,text="Obtener valor", command=mostrarEdad)
+boton.grid(row=1, column=0,padx=10, pady=10)
+ventana.mainloop()
